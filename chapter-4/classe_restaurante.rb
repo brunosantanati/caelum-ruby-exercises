eb021d601e670659dbef0322ea7636ff1dcd1ae6
Bruno Sant Ana <bruno.santana.ti@gmail.com>
2017-09-01 05:07:47 -0300

    Define initialize and instance variables

--- chapter-4/classe_restaurante.rb
+++ chapter-4/classe_restaurante.rb
@@ -1,11 +1,29 @@
 class Restaurante
-  def qualifica(nota, msg="Obrigado")
-    puts "A nota do restaurante foi #{nota}. #{msg}"
+  def initialize(nome)
+    puts "criando um novo restaurante: #{nome}"
+    @nome = nome
+  end
+
+  def qualifica(msg="Obrigado")
+    puts "A nota do #{@nome} foi #{@nota}. #{msg}"
+  end
+
+  #propriedades
+  def nota=(nota)
+    @nota = nota
+  end
+  
+  def nota
+    @nota
   end
 end
 
-restaurante_um = Restaurante.new
-restaurante_dois = Restaurante.new
+# crie dois nomes diferentes
+restaurante_um = Restaurante.new("Fasano")
+restaurante_dois = Restaurante.new("Fogo de Chao")
 
-restaurante_um.qualifica(10)
-restaurante_dois.qualifica(1, "Ruim!")
+restaurante_um.nota = 10
+restaurante_dois.nota = 1
+
+restaurante_um.qualifica
+restaurante_dois.qualifica("Comida Ruim!")
