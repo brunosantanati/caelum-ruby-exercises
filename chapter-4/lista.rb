ba251ab6a5830d69f4a179da6d0e8022f89c9382
Bruno Sant Ana <bruno.santana.ti@gmail.com>
2017-09-01 14:44:30 -0300

    Create class Franquia

--- chapter-4/lista.rb
+++ chapter-4/lista.rb
@@ -1,8 +1,33 @@
-nomes = []
+class Franquia
 
-nomes[0] = "Fasano"
-nomes << "Fogo de Chao"
+  def initialize
+    @restaurantes = []
+  end
 
-for nome in nomes
-  puts nome
+  def adiciona(restaurante)
+    @restaurantes << restaurante
+  end
+
+  def mostra
+    for restaurante in @restaurantes
+      puts restaurante.nome
+    end
+  end
+
 end
+
+class Restaurante
+  attr_accessor :nome
+end
+
+restaurante_um = Restaurante.new
+restaurante_um.nome = "Fasano"
+
+restaurante_dois = Restaurante.new
+restaurante_dois.nome = "Fogo de Chao"
+
+franquia = Franquia.new
+franquia.adiciona restaurante_um
+franquia.adiciona restaurante_dois
+
+franquia.mostra
