637a3461d4efa5ff4e5bb4d32a752e0f2969f5a9
Bruno Sant Ana <bruno.santana.ti@gmail.com>
2017-09-25 07:13:31 -0300

    Use Nokogiri to create a twitter reader

---
+++ chapter-15/twitter_reader.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'open-uri'
+require 'nokogiri'
+
+doc = Nokogiri::HTML(open('https://twitter.com/caelum'))
+items = doc.css ".content"
+items.each do |item|
+  autor = item.css(".fullname").first.content
+  tweet = item.css(".js-tweet-text").first.content
+
+  puts autor
+  puts tweet
+  puts
+end
